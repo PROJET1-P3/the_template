665c6e527435a3a82e084fbc2fa803bf05c6c589
NyAndoMayah <hei.mayah.3@gmail.com>
2023-07-31 17:06:12 +0300

    feat: permit serverless

--- src/main/java/com/example/the_template/TheTemplateApplication.java
+++ src/main/java/com/example/the_template/TheTemplateApplication.java
@@ -1,10 +1,17 @@
 package com.example.the_template;
 
+import com.example.the_template.controller.StudentController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({StudentController.class })
 public class TheTemplateApplication {
+    // silence console logging
+    @Value("${logging.level.root:OFF}")
+    String message = "";
 
     public static void main(String[] args) {
         SpringApplication.run(TheTemplateApplication.class, args);
